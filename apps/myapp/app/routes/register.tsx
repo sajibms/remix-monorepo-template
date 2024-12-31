be01d3eb29907e19a8322f5b853c4f9035eb0dd7
sajibms <shahadath.sajib.ms@gmail.com>
2024-12-31 10:55:09 +0600

    feat: add two app with two shared components

---
+++ apps/myapp/app/routes/register.tsx
@@ -0,0 +1,9 @@
+import { Register } from "@acme/register";
+
+export default function RegisterPage() {
+  return (
+    <div>
+      <Register />
+    </div>
+  )
+}
---
+++ libs/register/src/lib/register.tsx
@@ -0,0 +1,11 @@
+import styles from './register.module.css';
+
+export function Register() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Register!</h1>
+    </div>
+  );
+}
+
+export default Register;
